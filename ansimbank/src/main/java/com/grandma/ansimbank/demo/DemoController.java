bac3af7e4d468d7559f01ed7d343092d12692973
westjin <uko802@naver.com>
2025-09-03 14:45:35 +0900

    feat: stmop demo 컨트롤러 구현 (#9)

--- ansimbank/src/main/java/com/grandma/ansimbank/demo/DemoController.java
+++ ansimbank/src/main/java/com/grandma/ansimbank/demo/DemoController.java
@@ -7,13 +7,17 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/demo")
 @Validated
+@RequiredArgsConstructor
 public class DemoController {
+
+    private final SimpMessagingTemplate template;
 
     @PostMapping("/echo")
     public ResponseEntity<ApiCommonResponse<?>> echo(@RequestBody @Validated EchoRequest req) {
@@ -27,4 +31,12 @@ public class DemoController {
         @NotBlank(message = "text는 비어 있을 수 없습니다.") String  text,
         @Min(value = 1, message = "count는 1 이상이어야 합니다.") int count
     ) {}
+
+    @GetMapping("/send/{userId}")
+    public ResponseEntity<ApiCommonResponse<?>> sendToUser(@PathVariable String userId) {
+        String payload = "hello " + userId;
+        template.convertAndSendToUser(userId, "/queue/test", payload);
+        return ResponseEntity.ok(ApiCommonResponse.success(payload));
+    }
+
 }
